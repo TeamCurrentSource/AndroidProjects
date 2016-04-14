6354d6bf7cb078cda47f4df3d1f704bbc8f08e82
Antti Virtanen <Antti Virtanen>
2016-04-14 18:21:21 +0300

    Pari filuu

---
+++ app/src/main/java/com/teamcurrentsource/android/opensourcebookapplication/JsonVideoObject.java
@@ -0,0 +1,13 @@
+package com.teamcurrentsource.android.opensourcebookapplication;
+
+/**
+ * Created by Antti on 13.4.2016.
+ */
+public class JsonVideoObject {
+    public Urls download_urls;
+
+    public class Urls {
+        public String png;
+    }
+
+}
